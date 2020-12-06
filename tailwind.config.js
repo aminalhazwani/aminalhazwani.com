6e8bd46ae811f92b5c833244ab7388040034d7a3
Amin Al Hazwani <amin.alhazwani@gmail.com>
2020-12-06 12:02:55 +0100

    Add link arrows

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,24 @@ module.exports = {
   darkMode: "media",
   theme: {
     fontFamily: {
-      "sans": ["Inter", "Arial", "sans-serif"],
+      "sans": ["Helvetica Neue", "Arial", "sans-serif"],
+      "inter": ["Inter", "Arial", "sans-serif"],
     },
     screens: {
+      "sm": "375px",
       "md": "36rem",
       "lg": "64rem",
       "xl": "80rem",
+      "2xl": "1536px",
     },
   },
-  variants: {},
+  variants: {
+    extend: {
+      inset: ['group-hover'],
+      margin: ['group-hover'],
+      transitionProperty: ['group-hover'],
+    },
+  },
   plugins: [],
   purge: process.env.NODE_ENV === 'production' ? {
     enabled: true,
